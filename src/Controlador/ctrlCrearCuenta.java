6799f6c13bc6925bd0dac292619597f284520b60
Ale <aless070704@gmail.com>
2024-09-01 18:08:45 -0600

    Restricciones DUI valido, edad valida y contraseña valida

--- src/Controlador/ctrlCrearCuenta.java
+++ src/Controlador/ctrlCrearCuenta.java
@@ -29,40 +29,62 @@ public class ctrlCrearCuenta implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (e.getSource() == Vista.btnCrearCuenta) {
+    if (e.getSource() == Vista.btnCrearCuenta) {
+
+        Niveles nivelSeleccionado = (Niveles) Vista.jcmbTipoUsuario.getSelectedItem();
+        int idNivelSeleccionado = nivelSeleccionado.getId_nivelUsuario();
+
+        if (Vista.txtNombreUsuario.getText().isEmpty() ||
+            Vista.txtContrasenaUsuario.getText().isEmpty() || 
+            Vista.txtDuiUsuario.getText().isEmpty() || 
+            Vista.txtEdadUsuario.getText().isEmpty()) {
             
-            Niveles nivelSeleccionado = (Niveles) Vista.jcmbTipoUsuario.getSelectedItem();
-            int idNivelSeleccionado = nivelSeleccionado.getId_nivelUsuario();
+            JOptionPane.showMessageDialog(Vista, "Debes llenar todos los campos", "Error", JOptionPane.ERROR_MESSAGE);
+        
+        } else {
+            String contrasena = Vista.txtContrasenaUsuario.getText();
+            String edad = Vista.txtEdadUsuario.getText();
+            String dui = Vista.txtDuiUsuario.getText();
+
+            // Verificar la longitud, al menos 1 símbolo especial y 1 letra mayúscula
+            if (contrasena.length() < 8 ||
+                !contrasena.matches(".*[!@#$%^&*()_+=|<>?{}\\[\\]~-].*") ||
+                !contrasena.matches(".*[A-Z].*")) {
+                
+                JOptionPane.showMessageDialog(Vista, "La contraseña debe contener más de 8 caracteres, al menos 1 símbolo especial y 1 letra mayúscula", "Error", JOptionPane.ERROR_MESSAGE);
+
+            // Verificar que la edad no tenga más de 3 dígitos
+            } else if (edad.length() >= 3) {
+                JOptionPane.showMessageDialog(Vista, "Ingrese una edad valida", "Error", JOptionPane.ERROR_MESSAGE);
             
-            if (Vista.txtNombreUsuario.getText().isEmpty() ||
-                Vista.txtContrasenaUsuario.getText().isEmpty() || 
-                Vista.txtDuiUsuario.getText().isEmpty() || 
-                Vista.txtEdadUsuario.getText().isEmpty())
-            {
-                JOptionPane.showMessageDialog(Vista, "Debes llenar todos los campos", "Error", JOptionPane.ERROR_MESSAGE);
+            // Verificar que el DUI no tenga más de 10 dígitos
+            } else if (dui.length() > 10 || dui.length() < 10) {
+                JOptionPane.showMessageDialog(Vista, "El formato de DUI no es valido", "Error", JOptionPane.ERROR_MESSAGE);
+
             } else {
                 try {
-                ModeloUsuario.setNombre_usuario(Vista.txtNombreUsuario.getText());
-                ModeloUsuario.setContrasena_usuario(Vista.txtContrasenaUsuario.getText());
-                ModeloUsuario.setEdad_usuario(Integer.parseInt(Vista.txtEdadUsuario.getText()));
-                ModeloUsuario.setDUI_usuario(Vista.txtDuiUsuario.getText());
-                ModeloUsuario.setId_nivelUsuario(idNivelSeleccionado);
-                ModeloUsuario.Guardar();
-                
-                JOptionPane.showMessageDialog(Vista, "Cuenta creada con éxito", "Creación de cuenta", JOptionPane.INFORMATION_MESSAGE);
-                
-                LimpiarCampos();
-                } catch(NumberFormatException ex) {
+                    ModeloUsuario.setNombre_usuario(Vista.txtNombreUsuario.getText());
+                    ModeloUsuario.setContrasena_usuario(contrasena);
+                    ModeloUsuario.setEdad_usuario(Integer.parseInt(edad));
+                    ModeloUsuario.setDUI_usuario(dui);
+                    ModeloUsuario.setId_nivelUsuario(idNivelSeleccionado);
+                    ModeloUsuario.Guardar();
+
+                    JOptionPane.showMessageDialog(Vista, "Cuenta creada con éxito", "Creación de cuenta", JOptionPane.INFORMATION_MESSAGE);
+
+                    LimpiarCampos();
+                } catch (NumberFormatException ex) {
                     JOptionPane.showMessageDialog(Vista, "La edad debe ser un número válido", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         }
-        
-        if (e.getSource() == Vista.btnVerUsuarios){
-            Vista.dispose();
-            frmVerUsuarios.initfrmVerUsuarios();
-        }
     }
+
+    if (e.getSource() == Vista.btnVerUsuarios) {
+        Vista.dispose();
+        frmVerUsuarios.initfrmVerUsuarios();
+    }
+}
 
     @Override
     public void mousePressed(MouseEvent e) {
