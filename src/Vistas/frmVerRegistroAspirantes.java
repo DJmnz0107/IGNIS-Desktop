8d62940b798c0bd746af8292b5ef4884987c982a
Angel <20210086@ricaldone.edu.sv>
2024-09-21 22:17:56 -0600

    cambio de nombre

--- src/Vistas/frmVerRegistroAspirantes.java
+++ src/Vistas/frmVerRegistroAspirantes.java
@@ -10,12 +10,12 @@ import raven.drawer.Drawer;
  *
  * @author angel
  */
-public class frmRegistroAspirantes extends javax.swing.JFrame {
+public class frmVerRegistroAspirantes extends javax.swing.JFrame {
 
     /**
      * Creates new form frmRegistroAspirantes
      */
-    public frmRegistroAspirantes() {
+    public frmVerRegistroAspirantes() {
         initComponents();
     }
 
@@ -185,20 +185,21 @@ public class frmRegistroAspirantes extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmRegistroAspirantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmVerRegistroAspirantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmRegistroAspirantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmVerRegistroAspirantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmRegistroAspirantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmVerRegistroAspirantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmRegistroAspirantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmVerRegistroAspirantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmRegistroAspirantes().setVisible(true);
+                new frmVerRegistroAspirantes().setVisible(true);
             }
         });
     }
