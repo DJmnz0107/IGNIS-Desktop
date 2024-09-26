fed231764e5a6daaefd50a7c281694f6e936bfd2
Djimenez <20200093@ricaldone.edu.sv>
2024-09-25 21:23:41 -0600

    Estadisticas implementadas

---
+++ src/Vistas/ModelPieChart.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vistas;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Diego
+ */
+public class ModelPieChart {
+    
+        /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the values
+     */
+    public double getValues() {
+        return values;
+    }
+
+    /**
+     * @param values the values to set
+     */
+    public void setValues(double values) {
+        this.values = values;
+    }
+
+    /**
+     * @return the color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+     public ModelPieChart(String name, double values, Color color) {
+        this.name = name;
+        this.values = values;
+        this.color = color;
+    }
+
+    public ModelPieChart() {
+    }
+    
+    
+      private String name;
+    private double values;
+    private Color color; 
+    
+}
