2578e21ebf1e7716f0e42013cbe312a8c9de0f1a
Cesar Landaverde <20230355@ricaldone.edu.sv>
2024-10-03 22:41:46 -0600

    se termino el modelo

--- src/Modelo/MisionesRecursos.java
+++ src/Modelo/MisionesRecursos.java
@@ -4,6 +4,9 @@
  */
 package Modelo;
 import java.sql.*;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
 /**
  *
  * @author USUARIO
@@ -73,6 +76,73 @@ public class MisionesRecursos {
         }
     }
     
+    public void Eliminar(JTable tabla){
     
+        Connection conexion =ClaseConexion.getConexion();
+    int filaSeleccionada = tabla.getSelectedRow();
+    String miId = tabla.getValueAt(filaSeleccionada, 0).toString();
     
+    try {
+    
+        String sql="DELETE FROM Misiones_Recursos WHERE id_misionRecurso = ?";
+        PreparedStatement deleteMisionesRecu =conexion.prepareStatement(sql);
+        
+        int idMisioneRecu = Integer.parseInt(miId);
+        
+        deleteMisionesRecu.setInt(1,idMisioneRecu);
+        deleteMisionesRecu.executeUpdate();
+        
+        
+    } catch (Exception e) {
+        System.out.println("Este es el error en el método de eliminar: " + e);
+    }
+    }
+    public void Actualizar(){
+      Connection conexion = ClaseConexion.getConexion();
+      
+    try{
+        PreparedStatement updateMisionRecurso = conexion.prepareStatement("UPDATE Misiones_Recursos SET id_mision = ?, id_recurso = ? WHERE id_misionRecurso = ?");
+        
+        
+        updateMisionRecurso.setInt(1, getId_mision());
+        updateMisionRecurso.setInt(2,getId_recurso());
+        updateMisionRecurso.setInt(3,getId_misionRecurso());
+        
+        updateMisionRecurso.executeUpdate();
+    }catch (Exception e) {
+        System.out.println("este es el error en el metodo de actualizar" + e);
+    }
+    }
+    
+    public void Mostrar(JTable tabla){
+    Connection conexion =ClaseConexion.getConexion();
+    DefaultTableModel modeloDeDatos = new DefaultTableModel();
+    modeloDeDatos.setColumnIdentifiers(new Object[]{"id","Mision","Recurso"});
+    
+    try{
+    
+        Statement statement = conexion.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT M.descripcion_mision AS nombre_mision, " +
+             "R.nombre_recurso AS nombre_recurso " +
+             "FROM Misiones_Recursos MR " +
+             "INNER JOIN Misiones M ON MR.id_mision = M.id_mision " +
+             "INNER JOIN Recursos R ON MR.id_recurso = R.id_recurso;");
+        
+        while (rs.next()){
+        modeloDeDatos.addRow(new Object[]{
+          rs.getInt("id_misionRecurso"),
+          rs.getInt("id_mision"),
+          rs.getInt("id_recurso")
+        });
+        
+        tabla.setModel(modeloDeDatos);
+        }
+    }catch (Exception e) {
+            System.out.println("Este es el error en el modelo, metodo mostrar " + e);
+        }
+    
+    }
+    
+   
 }
+
